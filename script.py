b80acfd3c0243c32d8e7dd7c2ddde2eef5ea8732
Apenasgabs <Gabers357@gmail.com>
2021-06-03 23:59:02 -0300

    :rocket:

--- script.py
+++ script.py
@@ -2,13 +2,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.IN)
-print("Rodando script")
-sleep(0.4)
-print("liga ")
-GPIO.output(11,0)
-sleep(0.4)
-GPIO.output(11,1)
-print("desliga ") 
+print("Rodando script") 
 while (True):
     if(GPIO.input(13) == 1):
         GPIO.output(11, 0)
